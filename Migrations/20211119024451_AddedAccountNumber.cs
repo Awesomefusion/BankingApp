e6d5a74ea56b1fda7701d39e78bf4c78ae6ea9c5
Awesomefusion <53971809+Awesomefusion@users.noreply.github.com>
2021-11-19 02:45:11 +0000

    Added migration for AccountNumber

---
+++ Migrations/20211119024451_AddedAccountNumber.cs
@@ -0,0 +1,45 @@
+﻿using System;
+using Microsoft.EntityFrameworkCore.Migrations;
+
+#nullable disable
+
+namespace BankingApp.Migrations
+{
+    public partial class AddedAccountNumber : Migration
+    {
+        protected override void Up(MigrationBuilder migrationBuilder)
+        {
+            migrationBuilder.AlterColumn<Guid>(
+                name: "Id",
+                table: "Accounts",
+                type: "TEXT",
+                nullable: false,
+                oldClrType: typeof(int),
+                oldType: "INTEGER")
+                .OldAnnotation("Sqlite:Autoincrement", true);
+
+            migrationBuilder.AddColumn<int>(
+                name: "AccountNumber",
+                table: "Accounts",
+                type: "INTEGER",
+                nullable: false,
+                defaultValue: 0);
+        }
+
+        protected override void Down(MigrationBuilder migrationBuilder)
+        {
+            migrationBuilder.DropColumn(
+                name: "AccountNumber",
+                table: "Accounts");
+
+            migrationBuilder.AlterColumn<int>(
+                name: "Id",
+                table: "Accounts",
+                type: "INTEGER",
+                nullable: false,
+                oldClrType: typeof(Guid),
+                oldType: "TEXT")
+                .Annotation("Sqlite:Autoincrement", true);
+        }
+    }
+}
